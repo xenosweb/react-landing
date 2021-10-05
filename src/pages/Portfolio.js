fe2cf8632be166d61a9c235a12956521255a913d
coderaulia <auliasw89@gmail.com>
2021-10-05 23:08:56 +0700

    create json data file, and pass to Portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,14 +4,19 @@ import Header from "../parts/Header";
 import MiniHero from "../parts/MiniHero";
 import PortfolioItems from "../parts/PortfolioItems";
 import Footer from "../parts/Footer";
+import portfolio from "../data/portfolio.json";
 
 export default class Portfolio extends Component {
+	componentDidMount() {
+		window.title = "Portfolio &ndash; Coderaulia";
+		window.scrollTo(0, 0);
+	}
 	render() {
 		return (
 			<>
 				<Header></Header>
 				<MiniHero title="Portfolio" subtitle="by Coderaulia"></MiniHero>
-				<PortfolioItems></PortfolioItems>
+				<PortfolioItems data={portfolio}></PortfolioItems>
 				<CTA></CTA>
 				<Footer></Footer>
 			</>
